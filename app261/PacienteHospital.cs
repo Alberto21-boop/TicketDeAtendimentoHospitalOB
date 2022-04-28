ccecc3ed1ff94a386de980ef4360b9b2532f121e
alberto <albertobarbosa0003@gmail.com>
2022-04-28 08:51:32 -0300

    Adicionar arquivos de projeto.

---
+++ app261/PacienteHospital.cs
@@ -0,0 +1,13 @@
+﻿using app261;
+
+Paciente paciente = new Paciente();
+
+paciente.Nome = Console.ReadLine();
+paciente.Sobrenome = Console.ReadLine();
+paciente.Urgencia = Console.ReadLine();
+paciente.Chegada = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Nome : {paciente.NomePaciente()}");
+Console.WriteLine($"Sobrenome : {paciente.SobrenomePaciente()}");
+Console.WriteLine($"Urgencia : {paciente.UrgenciaAtendimento()}");
+Console.WriteLine($"Horas de Chegada : {paciente.HorasChegada()}");
---
+++ app261/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace app261
+{
+    internal class Paciente
+    {
+        public string Nome { get; set; } = default!;
+        public string Sobrenome { get; set; } = default!;
+        public string Urgencia { get; set; } = default!;
+        public int Chegada { get; set; }
+
+        public string NomePaciente()
+        {
+            return Nome;
+        }
+        public string SobrenomePaciente()
+        {
+            return Sobrenome;
+        }
+        public string UrgenciaAtendimento()
+        {
+            return Urgencia;
+        }
+        public int HorasChegada()
+        {
+            return Chegada;
+        }
+
+    }
+}
